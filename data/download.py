6fbd6ddc63ede571fc7c242be7e89e9f0b5a13fc
Bond Leung <bondgtm@gmail.com>
2025-11-05 10:22:39 -0600

    Updated download.py
    Quick script to test a bunch of urls to see if they contain a png and svg pair then download

--- data/download.py
+++ data/download.py
@@ -4,64 +4,75 @@ import json
 import os
 import re
 
+urls = [
+
+]
+
+# did up to pg 11...
+
 def main():
     print(len(os.listdir("data/test")))
     if len(os.listdir("data/test")) == 50:
         print(f"{len(os.listdir("data/test"))} files already downloaded.")
         return
     
-    URL = "https://musescore.com/classicman/scores/105780"
-    try:
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        }
+    index = 0
+    for URL in urls:
         
-        response = requests.get(URL, headers=headers, timeout=10)
-        
-        response.raise_for_status() 
-        
-    except requests.exceptions.RequestException as e:
-        print(f"Error downloading page: {e}")
-        return None
-
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-
-    scripts = soup.find_all('script')
-    script = scripts[8:9]
-    json_data = script[0].string.strip()
-    data = json.loads(json_data)
-    thumbnail_url = data.get('thumbnailUrl')
-    print(thumbnail_url)
-    first_link = soup.find_all('link')[0]
-    svg_link = first_link.get('href')
-    print(svg_link)
-
-    if "png" in thumbnail_url:
-        if "svg" in svg_link:
-            svg_dl = requests.get(svg_link, stream=True, timeout=10, headers=headers, allow_redirects=True)
-
-            name = data.get('name')
-            illegal_chars_pattern = r'[<>:"/\\|?*\x00-\x1F]'
-            name = re.sub(illegal_chars_pattern, "", name)
-            png_dl = requests.get(thumbnail_url, stream=True, timeout=10, headers=headers, allow_redirects=True)
-
-            # Create a directory in data/test/{name}/{name}.png
-            BASE_DATA_DIR = "data"
-            SUB_DIR = "test"
-            score_folder_path = os.path.join(BASE_DATA_DIR, SUB_DIR, name)
-            os.makedirs(score_folder_path, exist_ok=True)
-
-            with open(f"data/test/{name}/{name}.png", 'wb') as f:
-                f.write(png_dl.content)
-
-            with open(f"data/test/{name}/{name}.svg", 'wb') as f:
-                f.write(svg_dl.content)
-
-            print(f"Downloaded {name}.png and {name}.svg")
+        try:
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+            }
+            
+            response = requests.get(URL, headers=headers, timeout=10)
+            
+            response.raise_for_status() 
+            
+        except requests.exceptions.RequestException as e:
+            print(f"Error downloading page: {e}")
             return None
-    
-    print("No PNG, SVG pair found...")
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+
+        scripts = soup.find_all('script')
+        script = scripts[8:9]
+        json_data = script[0].string.strip()
+        data = json.loads(json_data)
+        thumbnail_url = data.get('thumbnailUrl')
+        print(thumbnail_url)
+        first_link = soup.find_all('link')[0]
+        svg_link = first_link.get('href')
+        print(svg_link)
+
+        if "png" in thumbnail_url:
+            if "svg" in svg_link:
+                svg_dl = requests.get(svg_link, stream=True, timeout=10, headers=headers, allow_redirects=True)
+
+                name = data.get('name')
+                illegal_chars_pattern = r'[<>:"/\\|?*\x00-\x1F]'
+                name = re.sub(illegal_chars_pattern, "", name)
+                png_dl = requests.get(thumbnail_url, stream=True, timeout=10, headers=headers, allow_redirects=True)
+
+                # Create a directory in data/test/{name}/{name}.png
+                BASE_DATA_DIR = "data"
+                SUB_DIR = "test"
+                score_folder_path = os.path.join(BASE_DATA_DIR, SUB_DIR, name)
+                os.makedirs(score_folder_path, exist_ok=True)
+
+                with open(f"data/test/{name}/{name}.png", 'wb') as f:
+                    f.write(png_dl.content)
+
+                with open(f"data/test/{name}/{name}.svg", 'wb') as f:
+                    f.write(svg_dl.content)
+
+                print(f"Downloaded {name}.png and {name}.svg")
+                index += 1
+                continue
+        
+        print("No PNG, SVG pair found...")
+        print(f"Finished index: {index}")
+        index += 1
 
 
 if __name__ == "__main__":
